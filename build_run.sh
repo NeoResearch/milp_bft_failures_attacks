a276e7a35e801355cc6aa2b969c4ecd45337575b
Vitor <vncoelho@gmail.com>
2020-06-15 15:16:08 -0300

    Minor changes on scripts

--- build_run.sh
+++ build_run.sh
@@ -4,7 +4,6 @@
 #./build_run.sh dbft2.0-liveness/dbft2.0_Byz_Liveness.mod data/4N_TMax.dat
 #./build_run.sh dbft3.0/MILP_BFTConsensus_dBFT3.0_2P.mod data/4N_TMax_dbft3.0.dat
 
-
 echo "Total Arguments:" $#
 echo "All Arguments values:" $@
 
@@ -20,8 +19,7 @@ mv temp.lp ./python-mip/
 echo "CALLING PYTHON-MIP";
 (cd ./python-mip/ && python3 test-python-mip.py temp.lp)
 
-echo "RENAMMING TEMP MODEL";
+echo "DELETING TEMP MODEL";
+rm python-mip/temp.lp
 
-mv python-mip/temp.lp python-mip/temp.old.lp
-
-echo "FINISHED";
+echo "FINISHED build_run.sh script";
--- python-mip/test-python-mip.py
+++ python-mip/test-python-mip.py
@@ -1,5 +1,6 @@
 from mip import *
 import sys
+import os.path
 
 file_name = sys.argv[1]
 
@@ -11,10 +12,11 @@ m.max_gap = 0.05
 m.read(file_name)
 print('model has {} vars, {} constraints and {} nzs'.format(m.num_cols, m.num_rows, m.num_nz))
 
-# Load MIP to speed-up process
-m.read("testToWrite.sol")
-
-print('validating mipstart {}'.format(m.validate_mip_start()))
+mip_start_filename="testToWrite.sol"
+if os.path.isfile(mip_start_filename):
+    print('\READING file for possible MIP_START')
+    m.read(mip_start_filename)
+    print('Validating mipstart {}\n\n'.format(m.validate_mip_start()))
 
 status = m.optimize(max_seconds=600)
 if status == OptimizationStatus.OPTIMAL:
@@ -24,14 +26,14 @@ elif status == OptimizationStatus.FEASIBLE:
 elif status == OptimizationStatus.NO_SOLUTION_FOUND:
     print('no feasible solution found, upper bound is: {}'.format(m.objective_bound))
 if status == OptimizationStatus.OPTIMAL or status == OptimizationStatus.FEASIBLE:
-    print('solution:')
+    print('\nsolution:')
     for v in m.vars:
        if abs(v.x) > 1e-6: # only printing non-zeros
           print('{} : {}'.format(v.name, v.x))
 
-m.write("testToWrite.sol")
-
-          
+print('\nWRITTING file possible MIP_START\n')
+m.write(mip_start_filename)
+    
 for k in range(m.num_solutions):
     print('Solution {} with Blocks {}'.format(k, m.objective_values[k]))
 #    m.sol[k].write("testToWrite{}.sol",k)
