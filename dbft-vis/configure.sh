ac14bbb3005a1b35bbb52ff077ea78b3b32a8141
Rodolfo Araujo <rodoufu@gmail.com>
2020-09-01 20:19:17 -0300

    Removing old code.

--- dbft-vis/configure.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sudo apt-get install -y python3-venv
-mkdir -p venv
-cd venv
-python3 -m venv env
-
-source venv/env/bin/activate
-pip3 install -r requirements.txt
-sudo apt-get install python3-lxml
